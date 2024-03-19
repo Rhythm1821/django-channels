55d438b87cd7652b8b786368a537f2b781073c5b
Rhythm1821 <alamrawat1821@gmail.com>
2024-03-19 22:19:57 +0530

    sync to async consumer

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,8 @@
-from django.urls import re_path
+from django.urls import re_path,path
 
 from . import consumers
 
 websockets_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$",consumers.ChatConsumer.as_asgi()),
+    path(r"ws/chat/<str:room_name>/",consumers.ChatConsumer.as_asgi()),
+    # re_path(r"ws/chat/(?P<room_name>\w+)/$",consumers.ChatConsumer.as_asgi()),
 ]
